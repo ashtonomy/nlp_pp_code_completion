2c943de2a3ce91b86cf5e83c4e4f299926031db6
Ashton Williamson <100785983+ashtonomy@users.noreply.github.com>
2023-09-04 21:49:36 -0400

    Update constants.py

--- constants.py
+++ constants.py
@@ -2,9 +2,4 @@ import os
 
 PARENT_DIR = os.path.dirname(__file__)
 SAVE_DIR = os.path.join(PARENT_DIR, "data")
-USERS=[
-    "ashtonomy", 
-    "VisualText", 
-    "ddehilster", 
-    "dehilsterlexis"
-]
+USERS=["ashtonomy"]
